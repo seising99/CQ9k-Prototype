65cfe9a3aa90411c8a4dcc50c2939182a7042f56
seising99 <shane.eising99@gmail.com>
2020-04-03 23:32:22 -0400

    Fixed Fullscreen Lag
    Setting framerate maximum fixed the bug, however keeping branch experimental until further testing and cleanup!!

--- bossDemos/HackModeState.cpp
+++ bossDemos/HackModeState.cpp
@@ -7,6 +7,7 @@ HackModeState::HackModeState()
 	WINDOW.create(sf::VideoMode(1920,1080), "CompuQuest9000 Demo", sf::Style::Fullscreen);
 	WINDOW.setView(sf::View(sf::FloatRect(0, 0, (float)WINDOW.getSize().x, (float)WINDOW.getSize().y)));
 	WINDOW.setMouseCursorVisible(false);
+	WINDOW.setFramerateLimit(60);
 }
 
 void HackModeState::input()
